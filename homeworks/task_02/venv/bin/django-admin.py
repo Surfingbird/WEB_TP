93da97fa3f9f0e570c92d978b191769a9c597a49
a.baronskiy <a.baronskiy@mail.ru>
2018-10-19 19:41:54 +0300

    add task_02 with templates

---
+++ homeworks/task_02/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrey/TECHNOPARK_SYSARC/SEM_01/WEB/homeworks/task_02/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
